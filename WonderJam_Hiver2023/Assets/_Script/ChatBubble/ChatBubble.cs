a4c33c82949ea933d7e65e56653a987f9f2439fe
Guillaume-Polverelli <75666966+Guillaume-Polverelli@users.noreply.github.com>
2023-02-25 04:38:06 +0100

    Add some features of chatbubbles

---
+++ WonderJam_Hiver2023/Assets/_Script/ChatBubble/ChatBubble.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChatBubble : MonoBehaviour
+{
+    private SpriteRenderer backgroundSpriteRenderer;
+    private SpriteRenderer iconSpriteRenderer;
+    private TextMeshPro textMeshPro;
+
+    private void Awake()
+    {
+        backgroundSpriteRenderer = transform.Find("Background").GetComponent<SpriteRenderer>();
+        iconSpriteRenderer = transform.Find("Icon").GetComponent<SpriteRenderer>();
+        textMeshPro = transform.Find("Text").GetComponent<TextMeshPro>();
+    
+    
+    }
+    private void Start()
+    {
+        Setup("Hello World");
+    }
+
+    private void Setup(string text)
+    {
+        textMeshPro.SetText(text);
+        textMeshPro.ForceMeshUpdate();
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
+        Vector2 padding = new Vector2(4f, 2f);
+        backgroundSpriteRenderer.size = textSize + padding;
+    }
+}
