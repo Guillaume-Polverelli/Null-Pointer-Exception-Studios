be0718724abdf1949b0fe786d0aebacd57350155
Guillaume-Polverelli <75666966+Guillaume-Polverelli@users.noreply.github.com>
2023-02-25 05:17:11 +0100

    Fix prefab player + add features chatbubble

--- WonderJam_Hiver2023/Assets/_Script/ChatBubble/ChatBubble.cs
+++ WonderJam_Hiver2023/Assets/_Script/ChatBubble/ChatBubble.cs
@@ -5,29 +5,40 @@ using TMPro;
 
 public class ChatBubble : MonoBehaviour
 {
+
+    public static void Create(Transform parent, Vector3 localPosition, string text)
+    {
+        //Instantiate()
+    }
     private SpriteRenderer backgroundSpriteRenderer;
-    private SpriteRenderer iconSpriteRenderer;
     private TextMeshPro textMeshPro;
+
+
 
     private void Awake()
     {
         backgroundSpriteRenderer = transform.Find("Background").GetComponent<SpriteRenderer>();
-        iconSpriteRenderer = transform.Find("Icon").GetComponent<SpriteRenderer>();
         textMeshPro = transform.Find("Text").GetComponent<TextMeshPro>();
     
     
     }
     private void Start()
     {
-        Setup("Hello World");
+        Setup("Hello World !");
     }
 
-    private void Setup(string text)
+    private void Setup( string text)
     {
         textMeshPro.SetText(text);
         textMeshPro.ForceMeshUpdate();
         Vector2 textSize = textMeshPro.GetRenderedValues(false);
-        Vector2 padding = new Vector2(4f, 2f);
+
+        Vector2 padding = new Vector2(3f, 3f);
         backgroundSpriteRenderer.size = textSize + padding;
+
+        Vector3 offset = new Vector3(-8f, 0f);
+        backgroundSpriteRenderer.transform.localPosition =
+            new Vector3(backgroundSpriteRenderer.size.x / 2f, 0f) + offset;
+
     }
 }
