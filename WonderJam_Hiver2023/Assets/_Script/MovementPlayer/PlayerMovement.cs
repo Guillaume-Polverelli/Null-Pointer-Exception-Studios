d7d2cd0f549c08d942e554cc1f6b01a4b2da091b
Guillaume-Polverelli <75666966+Guillaume-Polverelli@users.noreply.github.com>
2023-02-25 03:23:08 +0100

    Add scripts for fps movement + prefab player

--- WonderJam_Hiver2023/Assets/_Script/MovementPlayer/PlayerMovement.cs
+++ WonderJam_Hiver2023/Assets/_Script/MovementPlayer/PlayerMovement.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public CharacterController controller;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float gravity;
+
+    public Transform groundCheck;
+    [SerializeField] private float groundDistance;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +25,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
+        controller.Move(move * speed * Time.deltaTime);
 
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
