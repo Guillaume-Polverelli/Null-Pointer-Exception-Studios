44fca65040516442c1347648707d4ef55d727979
BOUGUET Theo <theo.bougue@gmail.com>
2023-02-26 01:25:37 -0500

    Parchemin

---
+++ WonderJam_Hiver2023/Assets/Parchemin.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Parchemin : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textMeshPro;
+    [SerializeField] private float velocityText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickedAccepterQuete()
+    {
+        StartCoroutine(Writer("ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED " +
+            "ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED ACCESS DENIED"));
+    }
+
+
+    public IEnumerator Writer(string text)
+    {
+        float pourcentage = 0f;
+        float textVelocity = (1f / text.Length);
+
+        while (pourcentage < 1f)
+        {
+
+            pourcentage = (pourcentage + textVelocity) * 5;
+            int nbCharacter = (int)(text.Length * pourcentage);
+
+            string h;
+            if (nbCharacter >= text.Length) h = text;
+            else h = text.Substring(0, nbCharacter);
+            textMeshPro.SetText(h);
+            yield return new WaitForSeconds(velocityText);
+        }
+
+        yield return new WaitForSeconds(Random.Range(1.5f, 3f));
+        Cursor.visible = false;
+        Destroy(gameObject);
+    }
+}
